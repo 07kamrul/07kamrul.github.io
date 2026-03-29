0899beb00b1ffb420c4ca5a06dd1fa863114179b
07kamrul <mdkamrulhasanewu@gmail.com>
2026-03-29 21:17:34 +0600

    deployment fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './',
   build: {
     outDir: 'docs',
     emptyOutDir: true,
